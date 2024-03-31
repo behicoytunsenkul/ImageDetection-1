28b6f1cd6b7f460ac0c2cdf42d25567dbf392811
BEHİÇ OYTUN ŞENKUL <114110417+behicoytunsenkul@users.noreply.github.com>
2024-03-31 14:46:47 +0300

    Initial Commit

---
+++ ImageDetection-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageDetection-1
+//
+//  Created by Oytun Şenkul on 31.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
